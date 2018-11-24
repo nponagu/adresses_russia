ec832f3272b3c7e70565734ec03413dc5aa2ad26
Alexander Tikhonov <nponagu@ya.ru>
2018-11-24 21:32:25 +0300

    A concept has changed :)

--- add_houses.py
+++ add_houses.py
@@ -1,247 +1,47 @@
-import dbf #working with dbf format
-from sqlalchemy import create_engine, Column, Integer, String, Date
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from db import db_session, Address, Status, CenterStatus, CurrentStatus, EstateStatus, FlatType, House, HouseStateStatus
-from db import IntervalStatus, DocType, NormativeDocument, OperStat, Room, RoomType, AddressObjectType, Stead, StructureStatus
-import os #library for working with folders
+import os
+from dbfread import DBF
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import Base
+from models import Address, Status, CenterStatus, CurrentStatus, EstateStatus, FlatType, House, HouseStateStatus, \
+    IntervalStatus, DocType, NormativeDocument, OperStat, Room, RoomType, AddressObjectType, Stead
 
-direction = 'W:/2018/Адреса/ФИАС/! archives/fias_delta_dbf'
+DIRECTION = '/fias_delta_dbf'
+MODEL_NAME = {
+    'ADDROB': Address,
+    'ACTSTAT': Status,
+    # 'ACTSTA': Status,
+    'CENTERST': CenterStatus,
+    'CURENTST': CurrentStatus,
+    'ESTSTAT': EstateStatus,
+    'FLATTYPE': FlatType,
+    'HOUSE': House,
+    'HSTSTAT': HouseStateStatus,
+    'INTVSTAT': IntervalStatus,
+    'NDOCTYPE': DocType,
+    'NORDOC': NormativeDocument,
+    'OPERSTAT': OperStat,
+    'ROOMTYPE': Room,
+    'SOCRBASE': RoomType,
+    'STEAD': AddressObjectType,
+    'STRSTAT': Stead
+}
 
-#add dbf to ADDROBJ
-def add_addres(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(
-            row["actstatus"], row["aoguid"], row["aoid"], row["aolevel"], row["areacode"], row["autocode"],    
-            row["centstatus"], row["citycode"], row["code"], row["currstatus"], row["enddate"],
-            row["formalname"], row["ifnsfl"], row["ifnsul"], row["nextid"], row["offname"], row["okato"],   
-            row["oktmo"], row["operstatus"], row["parentguid"], row["placecode"], row["plaincode"],   
-            row["postalcode"], row["previd"], row["regioncode"], row["shortname"], row["startdate"],   
-            row["streetcode"], row["terrifnsfl"], row["terrifnsul"], row["updatedate"], row["ctarcode"],    
-            row["extrcode"], row["sextcode"], row["livestatus"], row["normdoc"], row["plancode"],    
-            row["cadnum"], row["divtype"]
-            )
-        db_session.add(value)
+engine = create_engine("sqlite:///mybase.db")
 
-def add_status(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["actstatid"], row["name"])
-        db_session.add(value)
+Base.metadata.create_all(engine)
 
-def add_center_status(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["centerstid"], row["name"])
-        db_session.add(value)
+for file in os.listdir(DIRECTION):
+    print(
+        'Работаем с файлом "{}"'.format(file)
+    )
+    session = Session(bind=engine)
+    dp_path = r'{}/{}'.format(DIRECTION, file)
+    dbf_db = DBF(dp_path, 'cp866')
+    for row in dbf_db:
+        type_ = ''.join([i for i in os.path.splitext(file)[0] if not i.isnumeric()]) #в функцию
+        current_model = MODEL_NAME[type_]
+        session.add(current_model(**dict((k.lower(), v) for k, v in row.items())))  #в функцию
+    session.commit()
 
-def add_current_status(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["curentstid"], row["name"])
-        db_session.add(value)
-
-def add_estate_status(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["eststatid"], row["name"], row["shortname"])
-        db_session.add(value)
-
-def add_flat_type(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["fltypeid"], row["name"], row["shortname"])
-        db_session.add(value)
-
-def add_house(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(
-            row["aoguid"], row["buildnum"], row["enddate"], row["eststatus"], row["houseguid"], row["houseid"],
-            row["housenum"], row["statstatus"], row["ifnsfl"], row["ifnsul"], row["okato"], row["oktmo"], 
-            row["postalcode"], row["startdate"], row["strucnum"], row["strstatus"], row["terrifnsfl"], row["terrifnsul"],
-            row["updatedate"], row["normdoc"], row["counter"], row["cadnum"], row["divtype"]
-            )
-        db_session.add(value)
-
-def add_house_state_status(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["housestid"], row["name"])
-        db_session.add(value)
-
-def add_interval_status(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["intvstatid"], row["name"])
-        db_session.add(value)
-
-def add_doc_type(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["ndtypeid"], row["name"])
-        db_session.add(value)
-
-def add_normative_document(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(
-            row["normdocid"], row["docname"], row["docdate"], row["docnum"], row["doctype"], row["docimgid"]
-            )
-        db_session.add(value)
-
-def add_oper_stat(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["operstatid"], row["name"])
-        db_session.add(value)
-
-def add_room(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(
-            row["roomid"], row["roomguid"], row["houseguid"], row["regioncode"], row["flatnumber"],
-            row["flattype"], row["roomnumber"], row["roomtype"], row["cadnum"], row["roomcadnum"], row["postalcode"],
-            row["updatedate"], row["previd"], row["nextid"], row["operstatus"], row["startdate"], row["enddate"], 
-            row["livestatus"], row["normdoc"]
-            )
-        db_session.add(value)
-
-def add_room_type(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["rmtypeid"], row["name"], row["shortname"])
-        db_session.add(value)
-
-def add_address_object_type(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["level"], row["socrname"], row["scname"], row["kod_t_st"])
-        db_session.add(value)
-
-def add_stead(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(
-            row["steadguid"], row["number"], row["regioncode"], row["postalcode"], row["ifnsfl"], row["terrifnsfl"], 
-            row["ifnsul"], row["terrifnsul"], row["okato"], row["updatedate"], row["parentguid"], row["steadid"], 
-            row["previd"], row["operstatus"], row["startdate"], row["enddate"], row["nextid"], row["oktmo"],
-            row["livestatus"], row["cadnum"], row["divtype"], row["counter"], row["normdoc"]
-            )
-        db_session.add(value)
-
-def add_structure_status(db=None, db_session=None, class_table=None):
-    for row in db:
-        value = class_table(row["strstatid"], row["name"], row["shortname"])
-        db_session.add(value)
-
-#convert dbfs to sqlite
-addres_list = os.listdir(direction)
-for file in addres_list:
-    if len(file) > 6 and file[0:6] == 'ADDROB':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_addres(db=db, db_session=db_session, class_table=Address)
-        db.close()
-        db_session.commit()
-    elif len(file) > 6 and file[0:6] == 'ACTSTA':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_status(db=db, db_session=db_session, class_table=Status)
-        db.close()
-        db_session.commit()
-    elif len(file) > 8 and file[0:8] == 'CENTERST':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_center_status(db=db, db_session=db_session, class_table=CenterStatus)
-        db.close()
-        db_session.commit()
-    elif len(file) > 8 and file[0:8] == 'CURENTST':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_current_status(db=db, db_session=db_session, class_table=CurrentStatus)
-        db.close()
-        db_session.commit()
-    elif len(file) > 7 and file[0:7] == 'ESTSTAT':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_estate_status(db=db, db_session=db_session, class_table=EstateStatus)
-        db.close()
-        db_session.commit()
-    elif len(file) > 7 and file[0:8] == 'FLATTYPE':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_flat_type(db=db, db_session=db_session, class_table=FlatType)
-        db.close()
-        db_session.commit()
-    elif len(file) > 4 and file[0:5] == 'HOUSE':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_house(db=db, db_session=db_session, class_table=House)
-        db.close()
-        db_session.commit()
-    elif len(file) > 7 and file[0:7] == 'HSTSTAT':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_house_state_status(db=db, db_session=db_session, class_table=HouseStateStatus)
-        db.close()
-        db_session.commit()
-    elif len(file) > 8 and file[0:8] == 'INTVSTAT':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_interval_status(db=db, db_session=db_session, class_table=IntervalStatus)
-        db.close()
-        db_session.commit()
-    elif len(file) > 8 and file[0:8] == 'NDOCTYPE':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_doc_type(db=db, db_session=db_session, class_table=DocType)
-        db.close()
-        db_session.commit()
-    elif len(file) > 8 and file[0:6] == 'NORDOC':
-        db_name = direction + "/" + file
-        try:
-            db = dbf.Table(db_name, codepage='cp866', default_data_types='enhanced')
-            db.open()
-            add_normative_document(db=db, db_session=db_session, class_table=NormativeDocument)
-            db.close()
-            db_session.commit()
-        except KeyError:
-            print("KeyError" + db_name)
-    elif len(file) > 8 and file[0:8] == 'OPERSTAT':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_oper_stat(db=db, db_session=db_session, class_table=OperStat)
-        db.close()
-        db_session.commit()
-    elif len(file) > 4 and file[0:4] == 'ROOM' and file[0:5] != 'ROOMT':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_room(db=db, db_session=db_session, class_table=Room)
-        db.close()
-        db_session.commit()
-    elif len(file) > 8 and file[0:8] == 'ROOMTYPE':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_room_type(db=db, db_session=db_session, class_table=RoomType)
-        db.close()
-        db_session.commit()
-    elif len(file) > 8 and file[0:8] == 'SOCRBASE':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_address_object_type(db=db, db_session=db_session, class_table=AddressObjectType)
-        db.close()
-        db_session.commit()
-    elif len(file) > 5 and file[0:5] == 'STEAD':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_stead(db=db, db_session=db_session, class_table=Stead)
-        db.close()
-        db_session.commit()
-    elif len(file) > 7 and file[0:7] == 'STRSTAT':
-        db_name = direction + "/" + file
-        db = dbf.Table(db_name, codepage='cp866')
-        db.open()
-        add_structure_status(db=db, db_session=db_session, class_table=StructureStatus)
-        db.close()
-        db_session.commit()
+session.close()
--- models.py
+++ models.py
@@ -1,24 +1,11 @@
-import datetime
-from sqlalchemy import create_engine, Column, Integer, String, Date, DateTime
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey #этот тип нужен для создания внешнего ID зависимостей many to many
-
-engine = create_engine("sqlite:///mybase.db")
-#hop2
-
-db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
-Base.query = db_session.query_property() 
 
-# association_table = Table('association', Base.metadata,
-#     Column('room_id', Integer, ForeignKey('room.id')),
-#     Column('user_id', Integer, ForeignKey('user.id'))
-# )
 
 class Address(Base):
-    """Таблица – ADDROBJ (Object) содержит коды, наименования и типы адресообразующих элементов (регионы; округа; 
+    """Таблица – ADDROBJ (Object) содержит коды, наименования и типы адресообразующих элементов (регионы; округа;
     районы (улусы, кужууны); города, внутригородские районы,  поселки городского типа, сельские населенные пункты; 
     улицы, дополнительные адресные элементы, элементы улично-дорожной сети, планировочной структуры дополнительного 
     адресного элемента)."""
@@ -64,58 +51,9 @@ class Address(Base):
     cadnum = Column(Integer)
     divtype = Column(Integer)
 
-
-    def __init__(
-        self, actstatus = None,    aoguid = None,  aoid = None,    aolevel = None,     areacode = None,    autocode = None,    
-        centstatus = None,  citycode = None,    code = None,    currstatus = None,  enddate = None,     formalname = None,  
-        ifnsfl = None,  ifnsul = None,  nextid = None,  offname = None,     okato = None,   oktmo = None,   operstatus = None,  
-        parentguid = None,  placecode = None,   plaincode = None,   postalcode = None,  previd = None,  regioncode = None,  
-        shortname = None,   startdate = None,   streetcode = None,  terrifnsfl = None,  terrifnsul = None,  updatedate = None,  
-        ctarcode = None,    extrcode = None,    sextcode = None,    livestatus = None,  normdoc = None,     plancode = None,    
-        cadnum = None,  divtype = None
-        ):
-        self.actstatus = actstatus
-        self.aoguid = aoguid
-        self.aoid = aoid
-        self.aolevel = aolevel
-        self.areacode = areacode
-        self.autocode = autocode
-        self.centstatus = centstatus
-        self.citycode = citycode
-        self.code = code
-        self.currstatus = currstatus
-        self.enddate = enddate
-        self.formalname = formalname
-        self.ifnsfl = ifnsfl
-        self.ifnsul = ifnsul
-        self.nextid = nextid
-        self.offname = offname
-        self.okato = okato
-        self.oktmo = oktmo
-        self.operstatus = operstatus
-        self.parentguid = parentguid
-        self.placecode = placecode
-        self.plaincode = plaincode
-        self.postalcode = postalcode
-        self.previd = previd
-        self.regioncode = regioncode
-        self.shortname = shortname
-        self.startdate = startdate
-        self.streetcode = streetcode
-        self.terrifnsfl = terrifnsfl
-        self.terrifnsul = terrifnsul
-        self.updatedate = updatedate
-        self.ctarcode = ctarcode
-        self.extrcode = extrcode
-        self.sextcode = sextcode
-        self.livestatus = livestatus
-        self.normdoc = normdoc
-        self.plancode = plancode
-        self.cadnum = cadnum
-        self.divtype = divtype
-
     def __repr__(self):
         return '<Address {} {} {}>'.format(self.aoguid, self.formalname, self.regioncode)
+
 
 class Status(Base):
     """Статус актуальности ФИАС"""
@@ -124,14 +62,9 @@ class Status(Base):
     actstatid = Column(Integer)
     name = Column(String(100))
 
-    def __init__(
-        self, actstatid=None, name=None
-        ):
-        self.actstatid = actstatid
-        self.name = name
-
     def __repr__(self):
         return '<Status {} {}>'.format(self.actstatid, self.name)
+
 
 class CenterStatus(Base):
     """Статус центра"""
@@ -140,14 +73,9 @@ class CenterStatus(Base):
     centerstid = Column(Integer)
     name = Column(String(100))
 
-    def __init__(
-        self, centerstid=None, name=None
-        ):
-        self.centerstid = centerstid
-        self.name = name
-
     def __repr__(self):
         return '<Status {} {}>'.format(self.centerstid, self.name)
+
 
 class CurrentStatus(Base):
     """Таблица CURENTST (CurrentStatus) – содержит перечень статусов актуальности записи  адресного элемента по 
@@ -157,14 +85,9 @@ class CurrentStatus(Base):
     curentstid = Column(Integer)
     name = Column(String(100))
 
-    def __init__(
-        self, curentstid=None, name=None
-        ):
-        self.curentstid = curentstid
-        self.name = name
-
     def __repr__(self):
         return '<CurrentStatus {} {}>'.format(self.curentstid, self.name)
+
 
 class EstateStatus(Base):
     """Таблица ESTSTAT (EstateStatus) – содержит перечень возможных видов владений."""
@@ -174,15 +97,9 @@ class EstateStatus(Base):
     name = Column(String(100))
     shortname = Column(String(100))
 
-    def __init__(
-        self, eststatid=None, name=None, shortname=None
-        ):
-        self.eststatid = eststatid
-        self.name = name
-        self.shortname = shortname
-
     def __repr__(self):
         return '<EstateStatus {} {}>'.format(self.eststatid, self.name)
+
 
 class FlatType(Base):
     """Таблица FLATTYPE (FlatType) – НЕТ В ДОКУМЕНТЕ. ВАЖНО: ОПРЕДЕЛЯЕТ ТИП ПОМЕЩЕНИЯ (КВАРТИРА, ОФИС, СКЛАД И Т.Д.)"""
@@ -192,15 +109,9 @@ class FlatType(Base):
     name = Column(String(100))
     shortname = Column(String(100))
 
-    def __init__(
-        self, fltypeid=None, name=None, shortname=None
-        ):
-        self.fltypeid = fltypeid
-        self.name = name
-        self.shortname = shortname
-
     def __repr__(self):
         return '<FlatType {} {}>'.format(self.fltypeid, self.name)
+
 
 class House(Base):
     """Таблица HOUSE (House) содержат информацию о номерах отдельных домов, владений, домовладений, корпусов, 
@@ -231,38 +142,9 @@ class House(Base):
     cadnum = Column(String(100))
     divtype = Column(Integer)
 
-    def __init__(
-        self, aoguid=None, buildnum=None, enddate=None, eststatus=None, houseguid=None, houseid=None, housenum=None, 
-        statstatus=None, ifnsfl=None, ifnsul=None, okato=None, oktmo=None, postalcode=None, startdate=None, 
-        strucnum=None, strstatus=None, terrifnsfl=None, terrifnsul=None, updatedate=None, normdoc=None, counter=None,
-        cadnum=None, divtype=None
-        ):
-        self.aoguid = aoguid
-        self.buildnum = buildnum
-        self.enddate = enddate
-        self.eststatus = eststatus
-        self.houseguid = houseguid
-        self.houseid = houseid
-        self.housenum = housenum
-        self.statstatus = statstatus
-        self.ifnsfl = ifnsfl
-        self.ifnsul = ifnsul
-        self.okato = okato
-        self.oktmo = oktmo
-        self.postalcode = postalcode
-        self.startdate = startdate
-        self.strucnum = strucnum
-        self.strstatus = strstatus
-        self.terrifnsfl = terrifnsfl
-        self.terrifnsul = terrifnsul
-        self.updatedate = updatedate
-        self.normdoc = normdoc
-        self.counter = counter
-        self.cadnum = cadnum
-        self.divtype = divtype
-
     def __repr__(self):
         return '<House {} {} {}>'.format(self.aoguid, self.eststatus, self.housenum)
+
 
 class HouseStateStatus(Base):
     """Таблица HSTSTAT (HouseStateStatus) – содержит перечень возможных состояний объектов недвижимости"""
@@ -271,14 +153,9 @@ class HouseStateStatus(Base):
     housestid = Column(Integer)
     name = Column(String(60))
 
-    def __init__(
-        self, housestid=None, name=None
-        ):
-        self.housestid = housestid
-        self.name = name
-
     def __repr__(self):
         return '<HouseStateStatus {} {}>'.format(self.housestid, self.name)
+
 
 class IntervalStatus(Base):
     """Таблица INTVSTAT (IntervalStatus) – содержит перечень возможных значений интервалов домов 
@@ -288,12 +165,9 @@ class IntervalStatus(Base):
     intvstatid = Column(Integer)
     name = Column(String(100))
 
-    def __init__(self, intvstatid=None, name=None):
-        self.intvstatid = intvstatid
-        self.name = name
-
     def __repr__(self):
         return '<IntervalStatus {} {}>'.format(self.intvstatid, self.name)
+
 
 class DocType(Base):
     """DOCTYPE – Тип документа"""
@@ -302,12 +176,9 @@ class DocType(Base):
     ndtypeid = Column(Integer)
     name = Column(String(100))
 
-    def __init__(self, ndtypeid=None, name=None):
-        self.ndtypeid = ndtypeid
-        self.name = name
-
     def __repr__(self):
         return '<DocType {} {}>'.format(self.ndtypeid, self.name)
+
 
 class NormativeDocument(Base):
     """Таблица  NORDOC (NormativeDocument) - сведения по нормативному документу, являющемуся основанием 
@@ -321,16 +192,9 @@ class NormativeDocument(Base):
     doctype = Column(Integer)
     docimgid = Column(Integer)
 
-    def __init__(self, normdocid=None, docname=None, docdate=None, docnum=None, doctype=None, docimgid=None):
-        self.normdocid = normdocid
-        self.docname = docname
-        self.docdate = docdate
-        self.docnum = docnum
-        self.doctype = doctype
-        self.docimgid = docimgid
-
     def __repr__(self):
         return '<NormativeDocument {} {}>'.format(self.normdocid, self.docname)
+
 
 class OperStat(Base):
     """Таблица  OPERSTAT (OperationStatus) – содержит перечень кодов операций над адресными объектами"""
@@ -339,12 +203,9 @@ class OperStat(Base):
     operstatid = Column(Integer)
     name = Column(String(100))
 
-    def __init__(self, operstatid=None, name=None):
-        self.operstatid = operstatid
-        self.name = name
-
     def __repr__(self):
         return '<OperStat {} {}>'.format(self.operstatid, self.name)
+
 
 class Room(Base):
     """Таблица ROOM (Room) содержит записи с номерами помещений, квартир, офисов, комнат, 
@@ -371,31 +232,9 @@ class Room(Base):
     livestatus = Column(Integer)
     normdoc = Column(String(36))
 
-    def __init__(self, roomid=None, roomguid=None, houseguid=None, regioncode=None, flatnumber=None, flattype=None, 
-        roomnumber=None, roomtype=None, cadnum=None, roomcadnum=None, postalcode=None, updatedate=None, previd=None, 
-        nextid=None, operstatus=None, startdate=None, enddate=None, livestatus=None, normdoc=None):
-        self.roomid = roomid
-        self.roomguid = roomguid
-        self.houseguid = houseguid
-        self.regioncode = regioncode
-        self.flatnumber = flatnumber
-        self.flattype = flattype
-        self.roomnumber = roomnumber
-        self.roomtype = roomtype
-        self.cadnum = cadnum
-        self.roomcadnum = roomcadnum
-        self.postalcode = postalcode
-        self.updatedate = updatedate
-        self.previd = previd
-        self.nextid = nextid
-        self.operstatus = operstatus
-        self.startdate = startdate
-        self.enddate = enddate
-        self.livestatus = livestatus
-        self.normdoc = normdoc
-
     def __repr__(self):
         return '<Room {} {}>'.format(self.roomid, self.flatnumber, self.roomnumber)
+
 
 class RoomType(Base):
     """Таблица  ROOMTYPE (RoomType) – Тип комнаты"""
@@ -405,13 +244,9 @@ class RoomType(Base):
     name = Column(String(100))
     shortname = Column(String(100))
 
-    def __init__(self, rmtypeid=None, name=None, shortname=None):
-        self.rmtypeid = rmtypeid
-        self.name = name
-        self.shortname = shortname
-
     def __repr__(self):
         return '<RoomType {} {}>'.format(self.rmtypeid, self.name)
+
 
 class AddressObjectType(Base):
     """Таблица  SOCRBASE (AddressObjectType) – содержит перечень полных, сокращённых наименований типов адресных 
@@ -423,14 +258,9 @@ class AddressObjectType(Base):
     scname = Column(String(10))
     kod_t_st = Column(String(4))
 
-    def __init__(self, level=None, socrname=None, scname=None, kod_t_st=None):
-        self.level = level
-        self.socrname = socrname
-        self.scname = scname
-        self.kod_t_st = kod_t_st
-
     def __repr__(self):
         return '<AddressObjectType {} {}>'.format(self.kod_t_st, self.socrname)
+
 
 class Stead(Base):
     """Таблица STEAD (Stead) содержит коды, кадастровые номера земельных участков"""
@@ -460,38 +290,9 @@ class Stead(Base):
     counter = Column(Integer)
     normdoc = Column(String(36))
 
-    def __init__(
-        self, steadguid=None, number=None, regioncode=None, postalcode=None, ifnsfl=None, terrifnsfl=None,
-        ifnsul=None, terrifnsul=None, okato=None, updatedate=None, parentguid=None, steadid=None, previd=None,
-        operstatus=None, startdate=None, enddate=None, nextid=None, oktmo=None, livestatus=None, cadnum=None, 
-        divtype=None, counter=None, normdoc=None
-        ):
-        self.steadguid = steadguid
-        self.number = number
-        self.regioncode = regioncode
-        self.postalcode = postalcode
-        self.ifnsfl = ifnsfl
-        self.terrifnsfl = terrifnsfl
-        self.ifnsul = ifnsul
-        self.terrifnsul = terrifnsul
-        self.okato = okato
-        self.updatedate = updatedate
-        self.parentguid = parentguid
-        self.steadid = steadid
-        self.previd = previd
-        self.operstatus = operstatus
-        self.startdate = startdate
-        self.enddate = enddate
-        self.nextid = nextid
-        self.oktmo = oktmo
-        self.livestatus = livestatus
-        self.cadnum = cadnum
-        self.divtype = divtype
-        self.counter = counter
-        self.normdoc = normdoc
-
     def __repr__(self):
         return '<Stead {} {}>'.format(self.steadguid, self.number)
+
 
 class StructureStatus(Base):
     """Таблица STRSTAT (StructureStatus) – содержит перечень видов строений"""
@@ -501,13 +302,5 @@ class StructureStatus(Base):
     name = Column(String(20))
     shortname = Column(String(20))
 
-    def __init__(self, strstatid=None, name=None, shortname=None):
-        self.strstatid = strstatid
-        self.name = name
-        self.shortname = shortname
-
     def __repr__(self):
         return '<StructureStatus {} {}>'.format(self.strstatid, self.name)
-
-if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
